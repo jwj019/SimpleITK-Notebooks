97ad5653c745fce544d5d31c9d2ae42ce41d2bb5
Justin Johnson <jwj019@gmail.com>
2020-09-23 14:02:27 -0400

    Add loop for all notebooks

--- webdrive_static_pages.py
+++ webdrive_static_pages.py
@@ -5,14 +5,22 @@ from selenium import webdriver
 import psutil
 from selenium.common.exceptions import WebDriverException, NoSuchElementException
 
-current_dir = os.getcwd()
+files = []
 ipynb_dir = os.path.abspath('./Python')
 html_dir = os.path.abspath('./Python_html')
 
-print(html_dir)
+# for path in os.listdir(ipynb_dir):
+#     full_path = os.path.join(ipynb_dir, path)
+#     if os.path.isfile(full_path):
+#         files.append(full_path.replace("'", "\\'"))
+
+notebooks = [i for i in os.listdir(ipynb_dir) if i.endswith('.ipynb')]
+notebooks.sort()
+
+if not os.path.exists(html_dir):
+    os.makedirs(html_dir)
 
 notebook_run = Popen(["python3","-m","jupyter", "notebook","Python/00_Setup.ipynb", "--NotebookApp.password=''", "--NotebookApp.token=''", "--NotebookApp.iopub_msg_rate_limit=2000"], stdout=PIPE)
-
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
@@ -39,45 +47,39 @@ for counter in range(5):
             if proc.name() == PROCNAME:
                 proc.kill()        
         print("Retrying driver...")
+for notebook in notebooks: 
+    for counter in range(5):
+        try:
+            driver.get('http://localhost:8888/notebooks/' + notebook)
+            print("Got notebook...")
+            break
+        except WebDriverException:
+            #Cross platform
+            time.sleep(5)
+            print("Retrying browser...")
 
-for counter in range(5):
-    try:
-        driver.get('http://localhost:8888/notebooks/00_Setup.ipynb')
-        print("Got notebook...")
-        break
-    except WebDriverException:
-        #Cross platform
-        time.sleep(5)
-        print("Retrying browser...")
+    # time.sleep(5) # Let the user actually see something!
+    # driver.find_element_by_id("login_submit").click()
+    time.sleep(10)
+    cell_mnu = driver.find_element_by_id("celllink").click()
+    time.sleep(10)
+    run_cells = driver.find_element_by_id("run_all_cells").click()
+    for wait_counter in range(10):
+        try:
+            time.sleep(10)
+            print(driver.find_element_by_class_name("kernel_idle_icon"))
+            print(notebook + " done!")
+            break
+        except NoSuchElementException:
+            print("Waiting on kernel...")
 
-# time.sleep(5) # Let the user actually see something!
-# driver.find_element_by_id("login_submit").click()
-time.sleep(10)
-cell_mnu = driver.find_element_by_id("celllink").click()
-time.sleep(10)
-run_cells = driver.find_element_by_id("run_all_cells").click()
-for wait_counter in range(10):
-    try:
-        time.sleep(10)
-        print(driver.find_element_by_class_name("kernel_idle_icon"))
-        print("00_Setup.ipynb done!")
-        break
-    except NoSuchElementException:
-        print("Waiting on kernel...")
+    file_menu = driver.find_element_by_id("filelink").click()
+    time.sleep(10)
+    download_menu = driver.find_element_by_xpath("//a[text()='Download as']").click()
+    time.sleep(10)
+    download_html = driver.find_element_by_id("download_html").click()
+    time.sleep(10)
 
-file_menu = driver.find_element_by_id("filelink").click()
-time.sleep(10)
-download_menu = driver.find_element_by_xpath("//a[text()='Download as']").click()
-time.sleep(10)
-download_html = driver.find_element_by_id("download_html").click()
-# print(driver.get("http://localhost:8888/nbconvert/html/00_Setup.ipynb?download=true"))
-time.sleep(10)
-
-
-# search_box = driver.find_element_by_name('q')
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-# print(search_box)
 notebook_run.stdout.close()
 time.sleep(5) # Let the user actually see something!
 driver.quit()
